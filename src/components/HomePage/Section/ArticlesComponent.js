ca6e00aa2e648764d42e8404842419d3910c4d18
JeonMinJae <ssi02014@naver.com>
2021-02-24 05:35:31 +0900

    Add(minjae/HomeArticles): HomePage Articles UI 구현 29#

---
+++ src/components/HomePage/Section/ArticlesComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BsBookHalf } from "react-icons/bs";
+import { GiClothes } from "react-icons/gi";
+import { BsLaptop } from "react-icons/bs";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+const ArticlesComponent = () => {
+    return (
+        <section id="home-articles "className="home-articles">
+            <div className="container">
+                <h1 className="article-title">ARTICLES</h1>
+                <div className="article-box">
+                    <div className="article-item-box box1">
+                        <p className="article-item-icon"><BsBookHalf /></p>
+                        <h2 className="article-item-title">Book</h2>
+                        <FaArrowCircleRight className="article-btn" />
+                    </div>
+                    <div className="article-item-box box2">
+                        <p className="article-item-icon"><GiClothes /></p>
+                        <h2 className="article-item-title">Clothes</h2>
+                        <FaArrowCircleRight className="article-btn" />
+                    </div>
+                    <div className="article-item-box box3">
+                        <p className="article-item-icon"><BsLaptop /></p>
+                        <h2 className="article-item-title">Device</h2>
+                        <FaArrowCircleRight className="article-btn" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ArticlesComponent;
