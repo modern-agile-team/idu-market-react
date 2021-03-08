c95ee5f3bf70111ecdd15f38286bb09916d39fb0
JeonMinJae <ssi02014@naver.com>
2021-03-08 12:06:19 +0900

    Add(minjae/MarketBookList): Board Banner UI 구현 #46

---
+++ src/components/Boards/Book/BookListComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BoardItemData } from '../../../container/HeaderMenuData';
+const BookListComponent = () => {
+    return (
+        <section className="market" id="market">
+            <div className="container">
+                <h1>hi</h1>
+                <h1>hi</h1>
+                <h1>hi</h1>
+                <h1>hi</h1>
+                <h1>hi</h1>
+                <h1>hi</h1>
+                <h1>hi</h1>
+                <h1>hi</h1>
+            </div>
+        </section>
+    );
+};
+
+export default BookListComponent;
---
+++ src/container/BoardItemData.js
@@ -0,0 +1,17 @@
+export const BoardItemData = [
+    {
+        image: noticeImage1,
+        title: test,
+        content: test,
+    },
+    {
+        image: noticeImage2,
+        title: test,
+        content: test,
+    },
+    {
+        image: noticeImage3,
+        title: test,
+        content: test,
+    },
+]
