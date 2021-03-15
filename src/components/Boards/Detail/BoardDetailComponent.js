72d77d6ef8b9208b12479593d550682ba9bbf949
JeonMinJae <ssi02014@naver.com>
2021-03-15 11:54:07 +0900

    Add(minjae/boardDetail): boardDetail Setting #70

---
+++ src/components/Boards/Detail/BoardDetailComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BoardDetailComponent = () => {
+  return (
+    <>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+    </>
+  );
+};
+
+export default BoardDetailComponent;
---
+++ src/pages/Boards/Detail/BoardDetailPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BoardDetailComponent from "../../../components/Boards/Detail/BoardDetailComponent";
+
+const BoardDetailPage = () => {
+  return (
+    <>
+      <BoardDetailComponent></BoardDetailComponent>
+    </>
+  );
+};
+
+export default BoardDetailPage;
