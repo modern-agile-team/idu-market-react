54ed38db16c205cd979d8fffe574d40f9da487e9
JeonMinJae <ssi02014@naver.com>
2021-03-17 01:57:48 +0900

    Add(minjae/boardDetail): 댓글, 대댓글 생성 기능 구현 #70

--- src/components/Boards/Detail/CommentComponent.js
+++ src/components/Boards/Detail/CommentComponent.js
@@ -1,12 +1,17 @@
 import React, { useState, useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { COMMENT_GET_REQUEST, COMMENT_UPLOAD_REQUEST } from '../../../redux/types';
 
 import SingleComment from './SingleComment'
 
-const CommentComponent = ({}) => {
+const CommentComponent = ({ categoryName, num }) => {
+    const dispatch = useDispatch();
     const commentList = useSelector(state => state.comment.comments[0])
+    const userId = useSelector(state => state.loading.userId);
+
     const [formValue, setFormValue] = useState({
-        contents: "",
+        content: "",
+        studentId: userId,
     });
     
     const resetValue = useRef(null);
@@ -18,32 +23,69 @@ const CommentComponent = ({}) => {
         })
     }
 
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const {content, studentId} = formValue;
+        const body =  {
+            content,
+            studentId,
+            categoryName,
+            num,
+        }
+
+        console.log(body);
+
+        if (body.content.length === 0) {
+            alert("댓글이 비었습니다.");
+        } else {
+            dispatch({
+                type: COMMENT_UPLOAD_REQUEST,
+                payload: body,
+            });
+
+            dispatch({
+                type: COMMENT_GET_REQUEST,
+                payload: body,
+            })
+            resetValue.current.value = '';
+
+            setFormValue({
+                content: "",
+                studentId: userId,
+            });
+        }
+    }
     return (
         <>
             {commentList ? commentList.map((comment) => {
                 return (
-                    <SingleComment comment={comment} key={comment.commentNum} /> 
+                    <SingleComment 
+                        comment={comment} 
+                        key={comment.commentNum} 
+                        categoryName={categoryName}
+                        num={num}
+                    /> 
                 )
             }) : (
                 <></>
             )}
         
-
         <form className="detail-comment">
             <h1 className="comment-title">
                 Write comments
             </h1>
             <div className="comment-submit-box">
                 <textarea 
-                    // innerRef={resetValue}
+                    ref={resetValue}
                     type="textarea"
-                    name="contents"
+                    name="content"
                     id="comment-contents"
                     className="comment-contents"
                     onChange={onChange}
                     placeholder="Comment"
                 />
-                <button className="comment-submit-btn">
+                <button className="comment-submit-btn" onClick={onSubmit}>
                     Submit
                 </button>
             </div>
