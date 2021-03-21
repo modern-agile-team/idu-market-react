a54ccc154278694b7901163e649e6d8ca15c72a2
JeonMinJae <ssi02014@naver.com>
2021-03-21 16:59:02 +0900

    fix(minjae): scrollToTop component fix

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
+import { Component } from "react";
+import { withRouter } from "react-router";
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0)
+      window.scrollTo({
+        top: 0,
+        behavior: "auto",
+      });
     }
   }
 
   render() {
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export default withRouter(ScrollToTop)
+export default withRouter(ScrollToTop);
