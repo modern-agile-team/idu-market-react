58fb00f27ea89bee0330aa2ce4be6ebd00d0a230
JeonMinJae <ssi02014@naver.com>
2021-03-21 16:24:21 +0900

    fix(minjae/market): market list 반응형UI fix

--- src/components/Boards/Detail/BoardDetailTop.js
+++ src/components/Boards/Detail/BoardDetailTop.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { AiOutlineUser } from "react-icons/ai";
 import { BsCalendar } from "react-icons/bs";
-import { useDispatch, useSelector } from 'react-redux';
-import { BOARD_DELETE_REQUEST, IMAGE_DELETE_REQUEST } from '../../../redux/types';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  BOARD_DELETE_REQUEST,
+  IMAGE_DELETE_REQUEST,
+} from "../../../redux/types";
 
 const BoardDetailTop = ({ boardDetail, categoryName, num }) => {
   const dispatch = useDispatch();
-  const creatorId = useSelector(state => state.boards.studentId);
-  const userId = useSelector(state => state.auth.id);
+  const creatorId = useSelector((state) => state.boards.studentId);
+  const userId = useSelector((state) => state.auth.id);
 
   const [tradeStatus, setTradeStatue] = useState("판매중");
 
@@ -18,17 +21,16 @@ const BoardDetailTop = ({ boardDetail, categoryName, num }) => {
     const body = {
       url: [],
     };
-    
+
     let data = boardDetail.content;
 
-    while(true) {
+    while (true) {
       const matcher = data.match("<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>");
 
       if (matcher) {
         data = data.replace(matcher[0], "");
-      }
-      else break;
-      
+      } else break;
+
       imgList.push(matcher[1]);
     }
 
@@ -37,20 +39,20 @@ const BoardDetailTop = ({ boardDetail, categoryName, num }) => {
     if (body.url.length > 0) {
       dispatch({
         type: IMAGE_DELETE_REQUEST,
-        payload: body
-      })
+        payload: body,
+      });
     }
-  }
+  };
 
   const onTradeStatusClick = (e) => {
-      e.preventDefault();
-      setTradeStatue(e.target.textContent);
-    };
+    e.preventDefault();
+    setTradeStatue(e.target.textContent);
+  };
 
-  const onDelete = e => {
+  const onDelete = (e) => {
     e.preventDefault();
 
-    const confirm = window.confirm('정말 게시물을 삭제하시겠습니까?'); 
+    const confirm = window.confirm("정말 게시물을 삭제하시겠습니까?");
 
     if (confirm) {
       deleteImage();
@@ -60,67 +62,90 @@ const BoardDetailTop = ({ boardDetail, categoryName, num }) => {
         payload: {
           categoryName,
           num,
-        }
-      })
+        },
+      });
     }
-  }
+  };
 
   return (
-      <div className="detail-top-box">
-        {boardDetail ? (
-          <h1 className="detail-title">{boardDetail.title}</h1>
+    <div className="detail-top-box">
+      {boardDetail ? (
+        <h1 className="detail-title">{boardDetail.title}</h1>
+      ) : (
+        <></>
+      )}
+
+      {categoryName === "free" || categoryName === "notice" ? (
+        ""
+      ) : (
+        <p className="detail-price">{boardDetail.price}원</p>
+      )}
+
+      <div className="detail-btn-box">
+        {creatorId === userId ? (
+          <>
+            <Link
+              to={`/boards/${categoryName}/${num}/update`}
+              className="detail-btn-edit"
+            >
+              수정
+            </Link>
+            <button className="detail-btn-delete" onClick={onDelete}>
+              삭제
+            </button>
+            <Link to={`/boards/${categoryName}`} className="detail-btn-list">
+              목록
+            </Link>
+          </>
         ) : (
-          <></>
-        )}
-
-        {categoryName === 'free' || categoryName === 'notice' ? (
-            ""
-        ): (
-          <p className="detail-price">{boardDetail.price}원</p>
-        )}
-
-        <div className="detail-btn-box">
-          {creatorId === userId ? (
-            <>
-              <Link to={`/boards/${categoryName}/${num}/update`}className="detail-btn-edit">수정</Link>
-              <button className="detail-btn-delete" onClick={onDelete}>삭제</button>
-              <Link to={`/boards/${categoryName}`} className="detail-btn-list">목록</Link>
-            </>
-          ): (
-            <>
-              <Link to={`/boards/${categoryName}`} className="detail-btn-list">목록</Link>
-            </>
-          )}
-        </div>
-
-        <div className="detail-date-student">
-          <p><AiOutlineUser />&nbsp;{boardDetail.studentName}</p>
-          <p><BsCalendar />&nbsp;{boardDetail.inDate}</p>
-        </div>
-        
-        {categoryName === 'free' || categoryName === 'notice' ? (
-            ""
-        ): (
-          <div className="detail-trade-status-box">
-          <ul>
-            <li className="detail-trade-status">
-              {tradeStatus} <IoMdArrowDropdown />
-              <ul className="detail-trade-status-drop">
-                <li value="판매중" onClick={onTradeStatusClick}>
-                  판매중
-                </li>
-                <li value="예약중" onClick={onTradeStatusClick}>
-                  예약중
-                </li>
-                <li value="거래완료" onClick={onTradeStatusClick}>
-                  거래완료
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+          <>
+            <Link to={`/boards/${categoryName}`} className="detail-btn-list">
+              목록
+            </Link>
+          </>
         )}
       </div>
+
+      <div className="detail-date-student">
+        <p>
+          <AiOutlineUser />
+          &nbsp;{boardDetail.studentName}
+        </p>
+        <p>
+          <BsCalendar />
+          &nbsp;{boardDetail.inDate}
+        </p>
+      </div>
+
+      {categoryName === "free" || categoryName === "notice" ? (
+        <></>
+      ) : (
+        <>
+          {creatorId === userId ? (
+            <div className="detail-trade-status-box">
+              <ul>
+                <li className="detail-trade-status">
+                  {tradeStatus} <IoMdArrowDropdown />
+                  <ul className="detail-trade-status-drop">
+                    <li value="판매중" onClick={onTradeStatusClick}>
+                      판매중
+                    </li>
+                    <li value="예약중" onClick={onTradeStatusClick}>
+                      예약중
+                    </li>
+                    <li value="거래완료" onClick={onTradeStatusClick}>
+                      거래완료
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 
