d51b3effd741b97cd019654a2ea73c8722083c70
jsj <tw450497@gmail.com>
2021-03-29 13:15:05 +0900

    fix(sangjun/404) 404Page 반응형

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -6,15 +6,14 @@ function Page404() {
     return (
         <section className="nomatch" id="nomatch">
             <div className="container">
-                <div className="nomatch-text">
-                    <span className="nomatch-span">404</span> 
+                <div className="nomatch-text-box">
+                    <p className="nomatch-404">404</p> 
                     <p className="nomatch-p">Oops!</p>   
                     <p className="nomatch-p">해당 페이지가 존재하지 않거나 사용할 수 없습니다</p>
                 </div>
-                <div className="nomatch-buttonbox">
+                <div className="nomatch-button-box">
                     <Link to="/"><button className="nomatch-button" >홈으로 돌아가기</button></Link>
                 </div>
-
             </div>
         </section>
     )
