7c24373e6fa40ac465186b014e099ffe51527770
JeonMinJae <ssi02014@naver.com>
2021-04-01 10:17:04 +0900

    fix(minjae/tradeComplete): 구매 요청 인원 error fix

--- src/components/trade/TradeCompleteComponent.js
+++ src/components/trade/TradeCompleteComponent.js
@@ -84,9 +84,18 @@ const TradeCompleteComponent = (props) => {
         {buyers ? (
           <h1 className="trade-buyer-number">{`구매 요청 인원 (${(function () {
             let count = 0;
+
+            if (buyers.length === 1) {
+              if(buyers[0].nickname === nickname) {
+                return count = 0;
+              }
+            }
             for (let el in buyers) {
               if (el !== nickname) count++;
               return count;
+            }
+            if (buyers.length === 0) {
+              return count = 0;
             }
           })()})`}</h1>
         ) : (
