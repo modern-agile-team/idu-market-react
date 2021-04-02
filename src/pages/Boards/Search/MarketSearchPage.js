4357e4dccc6e8d9a33eafae62348826b153e7f93
JeonMinJae <ssi02014@naver.com>
2021-04-02 22:48:27 +0900

    Add(minjae/reactHelmet): keyword 추가 #129

--- src/pages/Boards/Search/MarketSearchPage.js
+++ src/pages/Boards/Search/MarketSearchPage.js
@@ -10,7 +10,7 @@ const MarketSearchPage = () => {
         <title>IUAM-(Search)</title>
         <meta charSet="utf-8" />
         <meta name="description" content="Idu Used Article Market" />
-        <meta name="keywords" content="IUAM, 인덕대학교, 아이두마켓, Idu, 중고시장, 중고마켓, 인덕대학교중고마켓, idu-market.shop, 검색, Search" />
+        <meta name="keywords" content="Idu Used Article Market, IUAM, 인덕대학교, 아이두마켓, Idu, 중고시장, 중고마켓, 인덕대학교중고마켓, idu-market.shop, 검색, Search" />
       </Helmet>
       <BoardBanner title="Market" desc="Search"></BoardBanner>
       <SearchListComponent></SearchListComponent>
