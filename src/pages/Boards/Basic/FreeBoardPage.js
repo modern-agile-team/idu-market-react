0f9f143477d3bf2d69f6d6d6a2ddf355558ae02e
JeonMinJae <ssi02014@naver.com>
2021-04-02 22:45:57 +0900

    Add(minjae/reactHelmet): keyword 추가 #129

--- src/pages/Boards/Basic/FreeBoardPage.js
+++ src/pages/Boards/Basic/FreeBoardPage.js
@@ -12,7 +12,7 @@ const BoardPage = () => {
             <title>IUAM-(Free)</title>
             <meta charSet="utf-8" />
             <meta name="description" content="Idu Used Article Market" />
-            <meta name="keywords" content="IUAM, 인덕대학교, 아이두마켓, Idu, 중고시장, 중고마켓, 인덕대학교중고마켓, idu-market.shop, 자유게시판" />
+            <meta name="keywords" content="Idu Used Article Market, IUAM, 인덕대학교, 아이두마켓, Idu, 중고시장, 중고마켓, 인덕대학교중고마켓, idu-market.shop, 자유게시판" />
         </Helmet>
             <BoardBanner title="Board" desc="Free" />
             <BasicBoardComponent categoryName="free"/>
--- src/pages/purchaseAndSaleList/PurchaseListPage.js
+++ src/pages/purchaseAndSaleList/PurchaseListPage.js
@@ -10,7 +10,7 @@ const PurchaseListPage = () => {
                 <title>IUAM-(PurchaseList)</title>
                 <meta charSet="utf-8" />
                 <meta name="description" content="Idu Used Article Market" />
-                <meta name="keywords" content="IUAM, 인덕대학교, 아이두마켓, Idu, 중고시장, 중고마켓, 인덕대학교중고마켓, idu-market.shop, 관심목록, 판매목록, 구매목록" />
+                <meta name="keywords" content="Idu Used Article Market, IUAM, 인덕대학교, 아이두마켓, Idu, 중고시장, 중고마켓, 인덕대학교중고마켓, idu-market.shop, 관심목록, 판매목록, 구매목록" />
             </Helmet>
             <BoardBanner title="IUAM" desc="Purchase"></BoardBanner>
             <PurchaseListComponent></PurchaseListComponent>
