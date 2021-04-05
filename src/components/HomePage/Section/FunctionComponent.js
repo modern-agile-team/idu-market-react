26771a35aca532f6b4f574b2260a78d4a81a1d42
JeonMinJae <ssi02014@naver.com>
2021-04-05 11:19:28 +0900

    Add(minjae/img): s3에서 img 불러오기

--- src/components/HomePage/Section/FunctionComponent.js
+++ src/components/HomePage/Section/FunctionComponent.js
@@ -27,19 +27,27 @@ const FunctionComponent = () => {
           }
         >
           <div className="function-item shopping">
-            <img src="https://woowahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/shopping-2.png" alt="중고거래" className="function-img" />
+            <img
+              src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/shopping.png"
+              alt="중고거래"
+              className="function-img"
+            />
             <p className="function-desc">Deal With Article</p>
           </div>
           <div className="function-item communication">
             <img
-              src="https://woowahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/communication-2.png"
+              src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/communication.png"
               alt="커뮤니케이션"
               className="function-img"
             />
             <p className="function-desc">Communication</p>
           </div>
           <div className="function-item information">
-            <img src="https://woowahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/information-2.png" alt="정보" className="function-img" />
+            <img
+              src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/information.png"
+              alt="정보"
+              className="function-img"
+            />
             <p className="function-desc">
               University <br />
               Information
